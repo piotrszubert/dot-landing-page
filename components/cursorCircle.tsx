e88dee156fd9a28095a4e4c5436550087c948753
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-28 13:39:55 +0000

    cursor circle

---
+++ components/cursorCircle.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import styles from '@/styles/Cursor.module.css';
+
+export default function CursorCircle() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (e: any) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      className={styles.cursor}
+      style={{ left: cursorPosition.x, top: cursorPosition.y }}
+    />
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import CursorCircle from '@/components/cursorCircle'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <CursorCircle />
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.grid}>
           <a
