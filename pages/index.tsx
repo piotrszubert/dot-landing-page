eea5df3e85dec1bb1147f4bea6302e5d0d822789
ps777 <56358194+piotrszubert@users.noreply.github.com>
2023-07-29 09:34:37 +0200

    changed font

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import { Montserrat } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import CursorCircle from '@/components/cursorCircle'
 
-const inter = Inter({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main} ${montserrat.className}`}>
         <h1>
           HELLO
         </h1>
